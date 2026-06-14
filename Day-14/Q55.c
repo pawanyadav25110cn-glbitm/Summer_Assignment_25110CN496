361b3a6374453bec46b35d87c23f8aa93efb27b9
Pawan Yadav <ay2573818@gmail.com>
2026-06-14 12:53:27 +0530

    Day-14 assignment uploaded

---
+++ Day-14/Q55.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+int main()
+{
+int arr[100], n, i;
+int largest, secondlargest;
+
+printf("Enter number of elements: ");
+scanf("%d", &n);
+
+printf("enter array elements:\n");
+for(i = 0; i<n; i++)
+{
+    scanf("%d", &arr[i]);
+}
+largest = secondlargest = arr[0];
+
+for(i = 1; i<n; i++)
+{
+    if(arr[i]>largest)
+    {
+        secondlargest = largest;
+        largest = arr[i];
+    }
+    else if(arr[i]> secondlargest && arr[i != largest])
+    {
+        secondlargest = arr[i];
+    }
+}
+printf("Second largest element = %d", secondlargest);
+return 0;
+}     
