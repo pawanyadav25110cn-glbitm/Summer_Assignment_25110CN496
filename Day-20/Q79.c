8f15c2c901dd447a0791b4763459b08f95e46b5e
Pawan Yadav <ay2573818@gmail.com>
2026-06-20 18:56:37 +0530

    Day-20 assignment uploaded

---
+++ Day-20/Q79.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main()
+{
+    int a[10][10], rows, cols;
+    int i, j, sum;
+
+    printf("Enter numbers of rows and columns: ");
+    scanf("%d %d", &rows, &cols);
+
+    printf("Enter matrix elements:\n");
+    for(i = 0; i<rows; i++)
+    {
+        for(j = 0; j<cols; j++)
+        {
+            scanf("%d", &a[i][j]);
+        }
+    }
+    printf("Row-wise sums are:\n");
+    for(i = 0; i<rows; i++)
+    {
+        sum = 0;
+        for(j = 0; j<cols; j++)
+        {
+            sum += a[i][j];
+        }
+        printf("Sum of row %d = %d\n", i + 1, sum);
+    }
+    return 0;
+}
